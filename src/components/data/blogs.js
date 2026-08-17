3a280cb93634b60a9189d5537359ccda367877ef
Shivamyadav231 <sy9670155278@gmail.com>
2026-08-17 16:10:32 +0530

    shivam

---
+++ src/components/data/blogs.js
@@ -0,0 +1,75 @@
+const blogs = [
+  {
+    id: "1",
+    title:
+      "Greater Noida West Double-Decker Flyover: Key Connectivity Boost",
+    image:
+      "https://static.wixstatic.com/media/d3987d_ab716aba96954446a868736333b1cc85~mv2.png/v1/fill/w_740,h_555,al_c,q_90,usm_0.66_1.00_0.01,enc_avif,quality_auto/d3987d_ab716aba96954446a868736333b1cc85~mv2.png",
+    description: "",
+  },
+
+  {
+    id: "2",
+    title: "",
+    image: "",
+    description: "",
+  },
+
+  {
+    id: "3",
+    title: "",
+    image: "",
+    description: "",
+  },
+
+  {
+    id: "4",
+    title: "",
+    image: "",
+    description: "",
+  },
+
+  {
+    id: "5",
+    title: "",
+    image: "",
+    description: "",
+  },
+
+  {
+    id: "6",
+    title: "",
+    image: "",
+    description: "",
+  },
+
+  {
+    id: "7",
+    title: "",
+    image: "",
+    description: "",
+  },
+
+  {
+    id: "8",
+    title: "",
+    image: "",
+    description: "",
+  },
+
+  {
+    id: "9",
+    title: "",
+    image: "",
+    description: "",
+  },
+
+  {
+    id: "10",
+    title: "",
+    image: "",
+    description: "",
+  },
+];
+
+export { blogs };
