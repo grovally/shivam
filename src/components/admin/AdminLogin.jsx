6550b853e89822d730a594a39c834f5572512bb0
Grovally <grovallypvtltd@gmail.com>
2026-08-31 13:59:49 +0530

    Update AdminLogin.jsx

--- src/components/admin/AdminLogin.jsx
+++ src/components/admin/AdminLogin.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const API = "https://chhhabra-2.onrender.com";
+const API = "https://chhhabra-1.onrender.com";
 
 export default function AdminLogin() {
 
