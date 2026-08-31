7afdb92807b7f8dbd3e888da5fe897dd93d6f302
Grovally <grovallypvtltd@gmail.com>
2026-08-31 13:58:47 +0530

    Update BlogManger.jsx

--- src/components/admin/BlogManger.jsx
+++ src/components/admin/BlogManger.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-const API =  "https://chhhabra-2.onrender.com";
+const API =  "https://chhhabra-1.onrender.com";
 
 export default function BlogManager() {
   const navigate = useNavigate();
